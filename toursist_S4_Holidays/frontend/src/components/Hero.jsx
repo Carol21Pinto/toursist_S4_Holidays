526d86568e5440d2ed7bd29379f88389048af361
spideyashith <fernandesashith24@gmail.com>
2026-05-27 15:10:28 +0530

    The CI/CD Pipline is Fixed

--- toursist_S4_Holidays/frontend/src/components/Hero.jsx
+++ toursist_S4_Holidays/frontend/src/components/Hero.jsx
@@ -42,7 +42,7 @@ function Hero() {
       >
       <div className="overlay">
         <p className="welcome-text">Explore • Experience • Enjoy</p>
-        <h1>Test Deploy Phase  </h1> 
+        <h1>S4 HOLIDAYS</h1> 
         <p className="tagline">
           Discover breathtaking destinations and unique travel experiences.
         </p>
